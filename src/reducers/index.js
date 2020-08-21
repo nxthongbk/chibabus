f5c217c1bace37ddc03afb923f205f72ad08ca4b
lephong334 <lephong334>
2020-08-21 11:23:52 +0700

    dashbard: buscounter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
-import devices from './device'
+import devices from './device';
+import buscounter from './buscounter';
 const myReducer = combineReducers({
-    devices
+    devices,
+    buscounter
 })
  
  export default myReducer;
