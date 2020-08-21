f3b1add84e7797a37b389145cee612ab4a3dcd51
pvthanh98 <57552257+pvthanh98@users.noreply.github.com>
2020-08-21 16:59:10 +0700

    Revert "Dashboard: cardinfo"

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -5,7 +5,6 @@ import ChartistGraph from "react-chartist";
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
-import DirectionBus from '@material-ui/icons/DirectionsBus';
 import Store from "@material-ui/icons/Store";
 import Warning from "@material-ui/icons/Warning";
 import DateRange from "@material-ui/icons/DateRange";
@@ -51,9 +50,15 @@ const useStyles = makeStyles(styles);
 function Dashboard(props) {
   const classes = useStyles();
 
-  const [totalCustomer, setTotalCustomer] = React.useState(0);
-  const [todayCustomer, setTodayCustomer] = React.useState(0);
-  const [customerLeftToday, setCustomerLeftToday] = React.useState(0);
+  const [id, setID] = React.useState("");
+  const [state, setState] = React.useState("");
+  const [image, setImage] = React.useState("");
+  const [lat, setLat] = React.useState(0);
+  const [long, setLong] = React.useState(0);
+  const [age, setAge] = React.useState(0);
+  const [gender, setGender] = React.useState(false);
+  const [bus, setBus] = React.useState("");
+  const [time, setTime] = React.useState("");
 
   useEffect(() => {
     getData();
@@ -61,55 +66,23 @@ function Dashboard(props) {
 
   const getData = async () => {
     var data = await axios().get('/buscounter');
+    console.log(data.data);
     props.updateBusCounter(data.data);
-    setTotalCustomer(Math.round(data.data.length/2));
-    customerToday(data.data);
-  }
-
-  const datesAreOnSameDay = (first, second) =>
-    first.getFullYear() === second.getFullYear() &&
-    first.getMonth() === second.getMonth() &&
-    first.getDate() === second.getDate();
-
-  const customerToday = (buscounter) => {
-    let today = new Date();
-    let current = 0;
-    let left = 0;
-    const arr = buscounter.map((customer, index) => {
-      let customerDate = new Date(customer.timestamp);
-      if (datesAreOnSameDay(today, customerDate)) {
-        if (customer.state == "up") {
-          current += 1;
-        } else {
-          left += 1;
-        }
-      }
-    });
-    setTodayCustomer(current);
-    setCustomerLeftToday(left);
-  }
-
-  //convert state from text to icon 
-  const customerState = (state) => {
-    if (state === "up") {
-      return <img src="/material-dashboard-react/images/get_on_bus.jpg" width="25px" alt="Up" />
-    }
-    return <img src="/material-dashboard-react/images/get_off_bus.jpg" width="25px" alt="Down" />;
   }
 
   const showBusCounter = () => {
     const a = props.buscounter && props.buscounter.map((customer, index) => {
-      return [
-        customer._id,
-        customerState(customer.state),
-        customer.image,
-        customer.lat,
-        customer.long,
-        customer.age,
-        customer.gender ? "Male" : "Female",
-        customer.device_id.license_plate,
-        timeFormat(customer.timestamp)
-      ]
+      
+
+      return [customer._id,
+              customer.state,
+              customer.image,
+              customer.lat,
+              customer.long,
+              customer.age,
+              customer.gender ? "Male" : "Female",
+              customer.device_id.license_plate,
+              timeFormat(customer.timestamp)]
     })
     return a;
   }
@@ -121,17 +94,21 @@ function Dashboard(props) {
           <Card>
             <CardHeader color="warning" stats icon>
               <CardIcon color="warning">
-                <DirectionBus />
+                <Icon>content_copy</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Today</p>
+              <p className={classes.cardCategory}>Used Space</p>
               <h3 className={classes.cardTitle}>
-                {todayCustomer}
+                49/50 <small>GB</small>
               </h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <Update />
-                Just now
+                <Danger>
+                  <Warning />
+                </Danger>
+                <a href="#pablo" onClick={e => e.preventDefault()}>
+                  Get more space
+                </a>
               </div>
             </CardFooter>
           </Card>
@@ -140,15 +117,15 @@ function Dashboard(props) {
           <Card>
             <CardHeader color="success" stats icon>
               <CardIcon color="success">
-                <Icon>arrow_upward</Icon>
+                <Store />
               </CardIcon>
-              <p className={classes.cardCategory}>Currently On Bus</p>
-              <h3 className={classes.cardTitle}>{todayCustomer - customerLeftToday}</h3>
+              <p className={classes.cardCategory}>Revenue</p>
+              <h3 className={classes.cardTitle}>$34,245</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <Update />
-                Just now
+                <DateRange />
+                Last 24 Hours
               </div>
             </CardFooter>
           </Card>
@@ -157,15 +134,15 @@ function Dashboard(props) {
           <Card>
             <CardHeader color="danger" stats icon>
               <CardIcon color="danger">
-                <Icon>arrow_downward</Icon>
+                <Icon>info_outline</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Customer Left</p>
-              <h3 className={classes.cardTitle}>{customerLeftToday}</h3>
+              <p className={classes.cardCategory}>Fixed Issues</p>
+              <h3 className={classes.cardTitle}>75</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <Update />
-                Just now
+                <LocalOffer />
+                Tracked from Github
               </div>
             </CardFooter>
           </Card>
@@ -176,13 +153,13 @@ function Dashboard(props) {
               <CardIcon color="info">
                 <Accessibility />
               </CardIcon>
-              <p className={classes.cardCategory}>Total customers</p>
-              <h3 className={classes.cardTitle}>{totalCustomer}</h3>
+              <p className={classes.cardCategory}>Followers</p>
+              <h3 className={classes.cardTitle}>+245</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
                 <Update />
-                Last 24 hours
+                Just Updated
               </div>
             </CardFooter>
           </Card>
@@ -199,6 +176,145 @@ function Dashboard(props) {
                 tableHeaderColor="primary"
                 tableHead={["ID", "State", "Image", "Latitude", "Longtitude", "Age", "Gender", "Bus", "Time"]}
                 tableData={showBusCounter()}
+              />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card chart>
+            <CardHeader color="success">
+              <ChartistGraph
+                className="ct-chart"
+                data={dailySalesChart.data}
+                type="Line"
+                options={dailySalesChart.options}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Daily Sales</h4>
+              <p className={classes.cardCategory}>
+                <span className={classes.successText}>
+                  <ArrowUpward className={classes.upArrowCardCategory} /> 55%
+                </span>{" "}
+                increase in today sales.
+              </p>
+            </CardBody>
+            <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> updated 4 minutes ago
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card chart>
+            <CardHeader color="warning">
+              <ChartistGraph
+                className="ct-chart"
+                data={emailsSubscriptionChart.data}
+                type="Bar"
+                options={emailsSubscriptionChart.options}
+                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+                listener={emailsSubscriptionChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Email Subscriptions</h4>
+              <p className={classes.cardCategory}>Last Campaign Performance</p>
+            </CardBody>
+            <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> campaign sent 2 days ago
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card chart>
+            <CardHeader color="danger">
+              <ChartistGraph
+                className="ct-chart"
+                data={completedTasksChart.data}
+                type="Line"
+                options={completedTasksChart.options}
+                listener={completedTasksChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Completed Tasks</h4>
+              <p className={classes.cardCategory}>Last Campaign Performance</p>
+            </CardBody>
+            <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> campaign sent 2 days ago
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={6}>
+          <CustomTabs
+            title="Tasks:"
+            headerColor="primary"
+            tabs={[
+              {
+                tabName: "Bugs",
+                tabIcon: BugReport,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0, 3]}
+                    tasksIndexes={[0, 1, 2, 3]}
+                    tasks={bugs}
+                  />
+                )
+              },
+              {
+                tabName: "Website",
+                tabIcon: Code,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0]}
+                    tasksIndexes={[0, 1]}
+                    tasks={website}
+                  />
+                )
+              },
+              {
+                tabName: "Server",
+                tabIcon: Cloud,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[1]}
+                    tasksIndexes={[0, 1, 2]}
+                    tasks={server}
+                  />
+                )
+              }
+            ]}
+          />
+        </GridItem>
+        <GridItem xs={12} sm={12} md={6}>
+          <Card>
+            <CardHeader color="warning">
+              <h4 className={classes.cardTitleWhite}>Employees Stats</h4>
+              <p className={classes.cardCategoryWhite}>
+                New employees on 15th September, 2016
+              </p>
+            </CardHeader>
+            <CardBody>
+              <Table
+                tableHeaderColor="warning"
+                tableHead={["ID", "Name", "Salary", "Country"]}
+                tableData={[
+                  ["1", "Dakota Rice", "$36,738", "Niger"],
+                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
+                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
+                  ["4", "Philip Chaney", "$38,735", "Korea, South"]
+                ]}
               />
             </CardBody>
           </Card>
