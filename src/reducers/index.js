845165060497a51b112dfdef2fd6c8dc292745d9
lephong334 <lephong334>
2020-09-18 11:08:37 +0700

    add page Customer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import devices from './device';
 import buscounter from './buscounter';
+import customers from './customers';
 import customeronday from './customeronday';
 import customeronmonth from './customeronmonth';
 import isLogin from './login';
@@ -8,6 +9,7 @@ import isLogin from './login';
 const myReducer = combineReducers({
     devices,
     buscounter,
+    customers,
     customeronday,
     customeronmonth,
     isLogin
